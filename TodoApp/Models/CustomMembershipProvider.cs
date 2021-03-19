cd048e1133d5602795494a0be7ae181d572f88e7
keisuke.nishida <keisuke.nishida@frontarc.co.jp>
2021-03-19 14:28:34 +0900

    認証情報のEntityFramework 完成

--- TodoApp/Models/CustomMembershipProvider.cs
+++ TodoApp/Models/CustomMembershipProvider.cs
@@ -107,15 +107,19 @@ namespace TodoApp.Models
 
         public override bool ValidateUser(string username, string password)
         {
-            if("administrator".Equals(username) && "password".Equals(password))
+            using (var db = new TodoesContext())
             {
-                return true;
 
+                var user = db.Users
+                    .Where(u => u.UserName == username && u.Password == password)
+                    .FirstOrDefault();
+
+                if (user != null)
+                {
+                    return true;
+                }
             }
-            if("user".Equals(username) && "password".Equals(password))
-            {
-                return true;
-            }
+           
             return false;
         }
     }
